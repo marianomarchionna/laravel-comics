9e843a24337cd9ae8efa58d0e5d5232309b0398e
Mariano <marianomarchionna8@gmail.com>
2021-10-27 17:28:18 +0200

    Header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,20 +1,20 @@
 <header>
     <div class="header-nav container">
         <div class="logo">
-            {{-- <img src="@/assets/img/dc-logo.png" alt="logo.png"> --}}
+            <a href="{{ route('pagina-home') }}"><img src="{{  asset('images/dc-logo.png') }}" alt="logo.png"></a>
         </div>
         <div class="nav-menu">
             <ul class="nav-items">
-                <li><a href="{{ route('pagina-characters') }}">CHARACTERS</a></li>
-                <li><a href="{{ route('pagina-comics') }}">COMICS</a></li>
-                <li><a href="{{ route('pagina-movies') }}">MOVIES</a></li>
-                <li><a href="{{ route('pagina-tv') }}">TV</a></li>
-                <li><a href="{{ route('pagina-games') }}">GAMES</a></li>
-                <li><a href="{{ route('pagina-collectibles') }}">COLLECTIBLES</a></li>
-                <li><a href="{{ route('pagina-videos') }}">VIDEOS</a></li>
-                <li><a href="{{ route('pagina-fans') }}">FANS</a></li>
-                <li><a href="{{ route('pagina-news') }}">NEWS</a></li>
-                <li><a href="{{ route('pagina-shop') }}">SHOP</a></li>
+                <li class="nav-item"><a href="{{ route('pagina-characters') }}">CHARACTERS</a></li>
+                <li class="nav-item"><a href="{{ route('pagina-comics') }}">COMICS</a></li>
+                <li class="nav-item"><a href="{{ route('pagina-movies') }}">MOVIES</a></li>
+                <li class="nav-item"><a href="{{ route('pagina-tv') }}">TV</a></li>
+                <li class="nav-item"><a href="{{ route('pagina-games') }}">GAMES</a></li>
+                <li class="nav-item"><a href="{{ route('pagina-collectibles') }}">COLLECTIBLES</a></li>
+                <li class="nav-item"><a href="{{ route('pagina-videos') }}">VIDEOS</a></li>
+                <li class="nav-item"><a href="{{ route('pagina-fans') }}">FANS</a></li>
+                <li class="nav-item"><a href="{{ route('pagina-news') }}">NEWS</a></li>
+                <li class="nav-item"><a href="{{ route('pagina-shop') }}">SHOP</a></li>
             </ul>
         </div>
     </div>
